8ccdc7cf89f5ab0c0f7ada80bbb7365cd5e699bb
Nabin Bhattarai <nabinbh363@gmail.com>
2016-07-28 21:13:33 -0700

    Added Text View

--- app/src/main/java/com/deeplock/currency/MainActivity.java
+++ app/src/main/java/com/deeplock/currency/MainActivity.java
@@ -2,12 +2,18 @@ package com.deeplock.currency;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mPlace;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlace = (TextView) findViewById(R.id.place);
+        mPlace.setText("Loading");
     }
 }
